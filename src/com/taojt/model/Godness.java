b41985a0ccb2473791beb422e319796ad14c56ab
taojt <taojt08@163.com>
2016-06-03 02:14:47 +0800

    add some files

---
+++ src/com/taojt/model/Godness.java
@@ -0,0 +1,21 @@
+package com.taojt.model;
+
+import java.util.Date;
+
+/**
+ * Created by taojt on 2016/6/3.
+ */
+public class Godness {
+    private Integer id;
+    private String username;
+    private int sex;
+    private int age;
+    private Date birthday;
+    private String email;
+    private String mobile;
+    private String create_user;
+    private String update_user;
+    private Date create_date;
+    private Date update_date;
+    private int isdel;
+}
